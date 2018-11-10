543a705bee7c93ab6e2504f57317f58f4c3be1bf
Hugo Laloge <hugo.laloge@epitech.eu>
2018-11-10 19:13:16 +0100

    feat(kallocator): begin basic K allocator

---
+++ kallocator/src/lib.rs
@@ -0,0 +1,326 @@
+#![feature(allocator_api)]
+#![feature(ptr_offset_from)]
+#![cfg_attr(not(test), no_std)]
+
+use core::alloc::{Alloc, AllocErr, Layout};
+use core::fmt;
+use core::marker::PhantomData;
+use core::mem::{size_of, transmute};
+use core::num::NonZeroUsize;
+use core::ptr::NonNull;
+
+pub struct KAllocator {
+    /// Start of the usable memory
+    memory_start: *mut u8,
+    /// End of the usable memory
+    memory_end: *mut u8,
+}
+
+impl KAllocator {
+    pub unsafe fn new(memory_start: NonNull<u8>, memory_end: NonNull<u8>) -> KAllocator {
+        let memory_start = memory_start.as_ptr();
+        let memory_end = memory_end.as_ptr();
+        let memory_size = memory_end.offset_from(memory_start);
+        assert!(
+            memory_size > size_of::<Block>() as isize,
+            "The memory should be at least {} bytes long",
+            size_of::<Block>()
+        );
+
+        let free_block = memory_start as *mut Block;
+        *free_block = Block::unused(memory_size as usize - size_of::<Block>());
+
+        KAllocator {
+            memory_start,
+            memory_end,
+        }
+    }
+
+    fn blocks(&self) -> impl Iterator<Item = &'static mut Block> {
+        BlockIterator::new(
+            unsafe { &mut *(self.memory_start as *mut Block) },
+            self.memory_end,
+        )
+    }
+
+    fn block_for(&self, layout: Layout) -> Option<&'static mut Block> {
+        // TODO Handle align
+        self.blocks().find(|b| !b.used && b.size() >= layout.size())
+    }
+
+    fn previous_block(&self, block: &Block) -> Option<&'static mut Block> {
+        self.blocks().find(|b| unsafe { b.next_block().is(block) })
+    }
+
+    fn is_in_memory(&self, block: &Block) -> bool {
+        block.address() != self.memory_end
+    }
+}
+
+unsafe impl Alloc for KAllocator {
+    unsafe fn alloc(&mut self, layout: Layout) -> Result<NonNull<u8>, AllocErr> {
+        let block = self.block_for(layout).ok_or(AllocErr)?;
+        block.used = true;
+
+        // Split if possible
+        // TODO Handle alignment
+        if block.size() > layout.size() + size_of::<Block>() {
+            // Reduce the size of the block and create a new one
+            let previous_size = block.size();
+            block.set_size(layout.size());
+
+            let next_block = block.next_block_mut();
+            next_block.set_size(previous_size - layout.size() - size_of::<Block>());
+            next_block.used = false;
+        }
+
+        Ok(NonNull::new_unchecked(block.data_address()))
+    }
+
+    unsafe fn dealloc(&mut self, ptr: NonNull<u8>, _layout: Layout) {
+        let block = Block::from_data_address(ptr.as_ptr());
+        block.used = false;
+
+        // Merge next block
+        let next_block = block.next_block();
+        if self.is_in_memory(next_block) && !next_block.used {
+            block.set_size(block.size() + next_block.size_with_header());
+        }
+
+        // Merge previous block
+        if let Some(previous_block) = self.previous_block(block) {
+            if !previous_block.used {
+                previous_block.set_size(previous_block.size() + block.size_with_header());
+            }
+        }
+    }
+}
+
+impl fmt::Debug for KAllocator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for block in self.blocks() {
+            write!(
+                f,
+                "[{}:{}]",
+                if block.used { "USED" } else { "FREE" },
+                block.size()
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+struct Block {
+    size: NonZeroUsize,
+    used: bool,
+}
+
+impl Block {
+    fn unused(size: usize) -> Block {
+        Block {
+            size: unsafe { NonZeroUsize::new_unchecked(size + size_of::<Self>()) },
+            used: false,
+        }
+    }
+
+    unsafe fn from_data_address(data: *mut u8) -> &'static mut Block {
+        &mut *(data.sub(size_of::<Self>()) as *mut Block)
+    }
+
+    /// Unsafe because the caller needs to check the next block is in the usable memory
+    unsafe fn next_block(&self) -> &'static Block {
+        &*(self.address().add(self.size_with_header()) as *mut Block)
+    }
+
+    unsafe fn next_block_mut(&mut self) -> &'static mut Block {
+        &mut *(self.address().add(self.size_with_header()) as *mut Block)
+    }
+
+    #[inline]
+    fn is(&self, other: &Block) -> bool {
+        self.address() == other.address()
+    }
+
+    fn set_size(&mut self, size: usize) {
+        self.size = unsafe { NonZeroUsize::new_unchecked(size + size_of::<Self>()) };
+    }
+
+    #[inline]
+    fn size(&self) -> usize {
+        self.size.get() - size_of::<Self>()
+    }
+
+    #[inline]
+    fn size_with_header(&self) -> usize {
+        self.size.get()
+    }
+
+    #[inline]
+    fn address(&self) -> *mut u8 {
+        unsafe { transmute(self) }
+    }
+
+    #[inline]
+    fn data_address(&self) -> *mut u8 {
+        unsafe { self.address().add(size_of::<Self>()) }
+    }
+}
+
+struct BlockIterator<'a> {
+    block: *mut Block,
+    memory_end: *mut u8,
+    marker: PhantomData<&'a Block>,
+}
+
+impl<'a> BlockIterator<'a> {
+    fn new(block: &'a mut Block, memory_end: *mut u8) -> Self {
+        BlockIterator {
+            block: block as *mut Block,
+            memory_end,
+            marker: PhantomData,
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        unsafe { (*self.block).address() == self.memory_end }
+    }
+}
+
+impl<'a> Iterator for BlockIterator<'a> {
+    type Item = &'a mut Block;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_at_end() {
+            return None;
+        }
+
+        unsafe {
+            let current_block = self.block.as_mut().unwrap();
+            self.block = current_block
+                .address()
+                .add(current_block.size_with_header()) as *mut Block;
+
+            Some(current_block)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn assert_block_count(allocator: &mut KAllocator, expected_count: usize) {
+        assert_eq!(
+            expected_count,
+            allocator.blocks().count(),
+            "Should have {} block at the init. Now: {:?}",
+            expected_count,
+            allocator
+        );
+    }
+
+    fn new_allocator(memory: &mut [u8]) -> KAllocator {
+        unsafe {
+            KAllocator::new(
+                NonNull::new_unchecked(&mut memory[0] as *mut u8),
+                NonNull::new_unchecked((&mut memory[0] as *mut u8).add(memory.len())),
+            )
+        }
+    }
+
+    #[test]
+    fn test_one_alloc() {
+        let mut memory = [0u8; 1024];
+
+        unsafe {
+            let memory_start = &mut memory[0] as *mut u8;
+            let mut allocator = new_allocator(&mut memory);
+
+            assert_block_count(&mut allocator, 1);
+
+            let layout = Layout::from_size_align(1, 1).unwrap();
+            let ptr = allocator.alloc(layout).expect("Should allocate");
+            assert_eq!(memory_start.add(size_of::<Block>()), ptr.as_ptr());
+            assert_block_count(&mut allocator, 2);
+
+            allocator.dealloc(ptr, layout);
+            assert_block_count(&mut allocator, 1);
+        };
+    }
+
+    #[test]
+    fn test_too_big_alloc() {
+        let mut memory = [0u8; 1024];
+
+        unsafe {
+            let mut allocator = new_allocator(&mut memory);
+
+            let layout = Layout::from_size_align(2048, 1).unwrap();
+            assert!(allocator.alloc(layout).is_err());
+        };
+    }
+
+    #[test]
+    fn test_merge_alloc() {
+        let mut memory = [0u8; 1024];
+        let memory_start = &mut memory[0] as *mut u8;
+
+        unsafe {
+            let mut allocator = new_allocator(&mut memory);
+
+            let layout = Layout::from_size_align(256, 1).unwrap();
+            let ptr1 = allocator.alloc(layout).expect("Should allocate");
+            let ptr2 = allocator.alloc(layout).expect("Should allocate");
+            let ptr3 = allocator.alloc(layout).expect("Should allocate");
+
+            assert_block_count(&mut allocator, 4);
+
+            assert_eq!(memory_start.add(size_of::<Block>()), ptr1.as_ptr());
+            assert_eq!(
+                memory_start
+                    .add(size_of::<Block>())
+                    .add(layout.size())
+                    .add(size_of::<Block>()),
+                ptr2.as_ptr()
+            );
+            assert_eq!(
+                memory_start
+                    .add(size_of::<Block>())
+                    .add(layout.size())
+                    .add(size_of::<Block>())
+                    .add(layout.size())
+                    .add(size_of::<Block>()),
+                ptr3.as_ptr()
+            );
+
+            allocator.dealloc(ptr1, layout);
+            allocator.dealloc(ptr3, layout);
+            allocator.dealloc(ptr2, layout);
+
+            assert_block_count(&mut allocator, 1);
+        };
+    }
+
+    #[test]
+    fn test_alloc_in_middle() {
+        let mut memory = [0u8; 1024];
+
+        unsafe {
+            let mut allocator = new_allocator(&mut memory);
+
+            let layout = Layout::from_size_align(256, 1).unwrap();
+            let _ptr1 = allocator.alloc(layout).expect("Should allocate");
+            let ptr2 = allocator.alloc(layout).expect("Should allocate");
+            let _ptr3 = allocator.alloc(layout).expect("Should allocate");
+
+            allocator.dealloc(ptr2, layout);
+
+            let ptr4 = allocator
+                .alloc(Layout::from_size_align(254, 1).unwrap())
+                .expect("Should allocate");
+            assert_eq!(ptr2, ptr4);
+        }
+    }
+}
